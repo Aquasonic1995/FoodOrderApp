31da6a623fc9138e4efad94bb2777485b29c72ce
Aquasonic1995 <Aquasonic1995@gmail.com>
2024-03-31 10:34:21 +0300

    feat: add layout and router

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -1,10 +1,19 @@
-import s from "./Button.module.scss";
+import s from './Button.module.scss';
 import {ButtonProps} from "./Button.props.ts";
 import cn from "clsx";
 import {FC} from "react";
-const Button:FC<ButtonProps> = ({children, onClick}) => {
+
+const Button: FC<ButtonProps> = ({
+                                     children,
+                                     appearance = 'small',
+                                     className,
+                                     ...props
+                                 }) => {
     return (
-        <button className={cn(s['button'], s['accent'])} onClick={onClick}>{children}</button>
+        <button className={cn(s['button'], s['accent'], className, {
+            [s['small']]: appearance === 'small',
+            [s['big']]: appearance === 'big'
+        })} {...props}>{children}</button>
     );
 };
 
