15ab9a66fb2dc8b77903ab2fa1d2a6cf25f5e5b6
Aquasonic1995 <Aquasonic1995@gmail.com>
2024-04-05 17:44:15 +0300

    feat:add login and add logout with dispatch

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -3,13 +3,10 @@ import Button from "../../Components/Button/Button.tsx";
 import {Link, useNavigate} from "react-router-dom";
 import s from './Login.module.scss';
 import Headling from "../../Components/Headling/Headling.tsx";
-import {FormEvent, useState} from "react";
-import axios, {AxiosError} from "axios";
-import {BASE_URL} from "../../helpers/API.ts";
-import {LoginResponse} from "../../Components/interfaces/auth.interface.ts";
-import {useDispatch} from "react-redux";
-import {userActions} from "../../store/user.slice.ts";
-import {AppDispatch} from "../../store/store.ts";
+import {FormEvent, useEffect, useState} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {login} from "../../store/user.slice.ts";
+import {AppDispatch, RootStore} from "../../store/store.ts";
 
 export type LoginFormType = {
     email: {
@@ -21,54 +18,51 @@ export type LoginFormType = {
 }
 
 const Login = () => {
+    const [a, setA] = useState('a');
     const [error, setError] =
         useState<string | null>(null);
     const navigate = useNavigate(); // Use useNavigate hook
     const dispatch = useDispatch<AppDispatch>();
+    const jwt = useSelector((s: RootStore) => s.user.jwt);
     const onFormSubmit = async (e: FormEvent) => {
         e.preventDefault();
         const target = e.target as typeof e.target & LoginFormType;
         const {email, password} = target;
         await sendLogin(email.value, password.value);
     };
+    useEffect(() => {
+        if(jwt){navigate('/');}
+    }, [jwt]);
     const sendLogin = async (email: string, password: string) => {
-        try {
-            setError(null);
-            const {data} = await axios.post<LoginResponse>(`${BASE_URL}/auth/login`, {
-                email,
-                password
-            });
-            dispatch(userActions.addJwt(data?.access_token));
-          navigate("/");
-        } catch (e) {
-            if (e instanceof AxiosError) {
-                setError(e.response?.data.message);
-            }
-        }
+        dispatch(login({email, password}));
     };
-        return (
-            <div className={s.login}>
-                <Headling>Вход</Headling>
-                <form className={s.form} onSubmit={onFormSubmit} name="form" >
-                    <div className={s.field}>
-                        <label htmlFor="email"></label>
-                        <Input id="email" name="email" placeholder="Email..."/>
+  const onButtonClick = (a:string) => () => {
+      console.log(a);
+  };
+
+    return (
+        <div className={s.login}>
+            <Headling>Вход</Headling>
+            <form className={s.form} onSubmit={onFormSubmit} name="form">
+                <div className={s.field}>
+                    <label htmlFor="email"></label>
+                    <Input id="email" name="email" placeholder="Email..."/>
+                </div>
+                <div className={s.field}>
+                    <label htmlFor="password"></label>
+                    <Input id="password" name="password" placeholder="Password..." type="password"/>
+                </div>
+                {error && <div className={s.error}>{error}</div>}
+                <Button appearance="big" onClick={onButtonClick(a)}>Вход</Button>
+                <div className={s.links}>
+                    <div>Нет аккаунта?</div>
+                    <div>
+                        <Link to="/auth/register">Регистрация</Link>
                     </div>
-                    <div className={s.field}>
-                        <label htmlFor="password"></label>
-                        <Input id="password" name="password" placeholder="Password..." type="password"/>
-                    </div>
-                    {error && <div className={s.error}>{error}</div>}
-                    <Button appearance="big">Вход</Button>
-                    <div className={s.links}>
-                        <div>Нет аккаунта?</div>
-                        <div>
-                            <Link to="/auth/register">Регистрация</Link>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        );
+                </div>
+            </form>
+        </div>
+    );
 
 };
 
--- src/store/user.slice.ts
+++ src/store/user.slice.ts
@@ -1,28 +1,49 @@
-import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice, PayloadAction} from "@reduxjs/toolkit";
 import {loadState} from "./storage.ts";
+import axios from "axios";
+import {LoginResponse} from "../Components/interfaces/auth.interface.ts";
+import {BASE_URL} from "../helpers/API.ts";
 
- export const JWT_STATE = 'userData';
+export const JWT_STATE = 'userData';
+
 export interface UserState {
-    jwt:string|null
-}
-export interface userPersistentState{
     jwt: string | null
 }
-const initialState:UserState ={
-    jwt:loadState<userPersistentState>(JWT_STATE)?.jwt ?? null
+
+export interface userPersistentState {
+    jwt: string | null
+}
+
+const initialState: UserState = {
+    jwt: loadState<userPersistentState>(JWT_STATE)?.jwt ?? null
 };
+export const login = createAsyncThunk('user/login',
+    async (params: { email: string, password: string }) => {
+        const {data} = await axios.post<LoginResponse>(`${BASE_URL}/auth/login`, {
+            email: params.email,
+            password: params.password
+        });
+        return data;
+    }
+);
 export const userSlice = createSlice({
-    name:'user',
+    name: 'user',
     initialState,
-    reducers:{
-        addJwt:(state, action:PayloadAction<string>)=>{
+    reducers: {
+        addJwt: (state, action: PayloadAction<string>) => {
             state.jwt = action.payload;
         },
-        logout:(state) =>{
+        logout: (state) => {
             state.jwt = null;
         },
+    },
+    extraReducers: (builder) => {
+        builder.addCase(login.fulfilled,
+            (state, action: PayloadAction<LoginResponse>) => {
+                state.jwt = action.payload.access_token;
+            });
     }
 });
 
 export default userSlice.reducer;
-export const userActions=userSlice.actions;
+export const userActions = userSlice.actions;
